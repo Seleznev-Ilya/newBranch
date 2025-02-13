e8273893ba810e2bd6624cc042960cf562af05d5
Selieznov <41084902+Seleznev-Ilya@users.noreply.github.com>
2025-02-13 10:43:02 +0100

    Update getHistoryBranch.js

--- components/select_history/getHistoryBranch.js
+++ components/select_history/getHistoryBranch.js
@@ -5,13 +5,14 @@ import { actionBorder } from "../utils/actionBorder.js";
 import { mainPlaceholder } from "../utils/main.js";
 
 export const getHistoryBranch = () => {
-  const isBranchName = branchName.checked;
+  const isNewBranchName = branchName.checked;
   const isFixPrefix = fixPrefix.checked;
   let lastBranch =
     containerHistory.options[containerHistory.selectedIndex].label;
-  const toCopyBranchName = `git checkout -b clean/${ isFixPrefix ? 'fix' : 'feature' }/${ lastBranch }`;
+  const toCopyNewBranchName = `git checkout -b clean/${ isFixPrefix ? 'fix' : 'feature' }/${ lastBranch }`;
+  const toCopyBranchName = `git checkout clean/${ isFixPrefix ? 'fix' : 'feature' }/${ lastBranch }`;
 
-  navigator.clipboard.writeText(isBranchName ? toCopyBranchName : lastBranch);
+  navigator.clipboard.writeText(isNewBranchName ? toCopyNewBranchName : toCopyBranchName);
 
   api.setItemToStart(lastBranch);
   addOptionsToMain();
